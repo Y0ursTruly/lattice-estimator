66385b82f395f14b3f36724d480a6fb612c4032d
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2025-10-13 08:52:07 -0400

    Improved sagemath installation

--- estimate.sh
+++ estimate.sh
@@ -25,19 +25,32 @@ if ! command -v python3 >/dev/null 2>&1; then
 fi
 
 # sagemath installer
-if ! command -v conda >/dev/null 2>&1; then
+if apt show sagemath 2>/dev/null | grep -q "Package: sagemath"; then
     echo "Installing sagemath via package manager"
     $PKG_INSTALL sagemath
-    #lines below commented out
-    #curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
-    #bash miniconda.sh -b -p $HOME/miniconda
-    #rm miniconda.sh
-    #export PATH="$HOME/miniconda/bin:$PATH"
+
+elif ! command -v conda >/dev/null 2>&1; then
+    echo "Installing sagemath via conda"
+    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+    rm miniconda.sh
+    export PATH="$HOME/miniconda/bin:$PATH"
     $HOME/miniconda/bin/conda init bash || true
-    #source ~/.bashrc || true
+    source ~/.bashrc || true
+    eval "$(conda shell.bash hook)"
     # shell reloaded to make sure conda was initialised
-    #echo "Finished installing conda"
-fi
+    conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+    conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+    echo "Finished installing conda"
+   conda config --add channels conda-forge || true
+    conda config --add channels defaults || true
+    conda config --set channel_priority flexible || true
+    echo "Configuring sagemath"
+    conda create -n sage-env -c conda-forge sage --yes --quiet
+    eval "$(conda shell.bash hook)"
+    conda activate sage-env
+    echo "Configuration of sagemath complete"
+
 elif command -v conda >/dev/null 2>&1; then
     echo "Installing sagemath via conda"
     conda config --add channels conda-forge || true
