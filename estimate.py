04bf66c1cf27ebda617665e671e2418e35803b95
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2025-11-15 19:48:43 +0000

    faster lattice configuration

--- estimate.py
+++ estimate.py
@@ -3,9 +3,13 @@ from estimator.nd import UniformMod, DiscreteGaussian
 from estimator import LWE, schemes #schemes are in estimator/schemes.py
 from math import log2
 
-N = int(32768/2)
-q = 1329227995775244468652735166391779329 #a sample product of 2 [60,60] primes
+#N = int(32768/2)
+#q = 1329227995775244468652735166391779329 #a sample product of 2 [60,60] primes
 #these primes were: [1152921504598720513, 1152921504606584833]
+
+N = int(32768/4)
+q = 576460752302473217
+#yes, 1152921504606584833, the 60 bit prime makes the construction weaker than the 59 bit one
 
 Xs = UniformMod(3)
 Xe = DiscreteGaussian(stddev=3.19) #SEAL default sigma is 3.19
@@ -14,8 +18,8 @@ Xe = DiscreteGaussian(stddev=3.19) #SEAL default sigma is 3.19
 
 my_config = LWEParameters(n=N, q=q, Xs=Xs, Xe=Xe)
 print("Running Estimator...")
-est = LWE.estimate.rough(my_config) # I need to find a machine with enough RAM that this doesn't get terminated
-#est = LWE.estimate(schemes.Kyber512) # example that doesn't get terminated on github codespaces
+est = LWE.estimate.rough(my_config) # 512 bit secure configuration currently
+#est = LWE.estimate(schemes.Kyber512) # normal configuration example (used to test that this code works properly)
 best = min(est.values(), key=lambda x: x['rop'])
 print("===========================")
 print(best)
