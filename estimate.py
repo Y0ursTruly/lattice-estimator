1113e2f46bf5f9039f9e4870614017f502b076bf
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2026-04-27 14:36:39 +0000

    migration to custom Module Learning With Errors configuration

--- estimate.py
+++ estimate.py
@@ -4,21 +4,30 @@ from estimator import LWE, schemes #schemes are in estimator/schemes.py
 from math import log2
 from estimator.reduction import RC, ADPS16
 
-N = int(2048)
-q = 268369921 #28 bit prime
-#rough 229, but full estimate 251 bit security
+#N = 2048
+#q = 268369921 #28 bit prime
 
-Xs = UniformMod(3)
-Xe = DiscreteGaussian(stddev=3.19) #SEAL default sigma is 3.19
+#Xs = UniformMod(3)
+#Xe = DiscreteGaussian(stddev=3.19) #SEAL default sigma is 3.19
 #according to https://www.microsoft.com/en-us/research/wp-content/uploads/2017/06/sealmanual_v2.2.pdf
 #and previously: https://www.microsoft.com/en-us/research/wp-content/uploads/2016/09/sealmanual.pdf
+# this was for microsoft SEAL BFV stuff of a standard deviation 3.19 for Xe and UniformMod(3)
+# but we're moving on from SEAL onto a module-lwe configuration for speed
 
-bfv_scheme = LWEParameters(n=N, q=q, Xs=Xs, Xe=Xe)
+N = 1024
+q = 8191
+
+Xs = DiscreteGaussian(stddev=3.937) #KYBER_ETA1=31 in params.h
+Xe = DiscreteGaussian(stddev=3.937) #KYBER_ETA2=31 in params.h
+# module learning with errors scheme intended to be adopted from the ind-cpa parts of Kyber1024
+
+
+my_mlwe_scheme = LWEParameters(n=N, q=q, Xs=Xs, Xe=Xe)
 print("Running Estimator...")
-bfv_estimate = LWE.estimate(bfv_scheme, red_cost_model=ADPS16(mode="quantum"))
+my_mlwe_estimate = LWE.estimate(my_mlwe_scheme, red_cost_model=ADPS16(mode="quantum"))
 kyber_estimate = LWE.estimate(schemes.Kyber1024, red_cost_model=ADPS16(mode="quantum"))
-best_bfv = min(bfv_estimate.values(), key=lambda x: x['rop'])
+best_my_mlwe = min(my_mlwe_estimate.values(), key=lambda x: x['rop'])
 best_kyber = min(kyber_estimate.values(), key=lambda x: x['rop'])
 print("===========================")
-print("BFV Security Estimate:", log2(best_bfv['rop']))
+print("My MLWE Security Estimate:", log2(best_my_mlwe['rop']))
 print("Kyber Security Estimate:", log2(best_kyber['rop']))
--- estimate.sh
+++ estimate.sh
@@ -25,11 +25,12 @@ if ! command -v python3 >/dev/null 2>&1; then
 fi
 
 # sagemath installer
-if apt show sagemath 2>/dev/null | grep -q "Package: sagemath"; then
-    echo "Installing sagemath via package manager"
-    $PKG_INSTALL sagemath
+#if apt show sagemath 2>/dev/null | grep -q "Package: sagemath"; then
+    #echo "Installing sagemath via package manager"
+    #$PKG_INSTALL sagemath
 
-elif ! command -v conda >/dev/null 2>&1; then
+#elif ! command -v conda >/dev/null 2>&1; then
+if ! command -v conda >/dev/null 2>&1; then
     echo "Installing sagemath via conda"
     curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
     bash miniconda.sh -b -p $HOME/miniconda
@@ -41,35 +42,34 @@ elif ! command -v conda >/dev/null 2>&1; then
     # shell reloaded to make sure conda was initialised
     conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
     conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
-    echo "Finished installing conda"
+    echo "Configuring sagemath"
     conda config --add channels conda-forge || true
     conda config --add channels defaults || true
     conda config --set channel_priority flexible || true
-    echo "Configuring sagemath"
     conda create -n sage-env -c conda-forge sage --yes --quiet
     eval "$(conda shell.bash hook)"
     conda activate sage-env
+    touch "sage-configured.txt"
     echo "Configuration of sagemath complete"
 
-elif [ ! -f "sage-configured.txt" ] command -v conda >/dev/null 2>&1; then
-    echo "Installing sagemath via conda"
+elif [ ! -f "sage-configured.txt" ] && command -v conda >/dev/null 2>&1; then
+    echo "Configuring sagemath"
     conda config --add channels conda-forge || true
     conda config --add channels defaults || true
     conda config --set channel_priority flexible || true
-    echo "Configuring sagemath"
     conda create -n sage-env -c conda-forge sage --yes --quiet
     echo "Configuration of sagemath complete"
     eval "$(conda shell.bash hook)"
     conda activate sage-env
+    touch "sage-configured.txt"
+    echo "Configuration of sagemath complete"
 fi
 
 # pip3 install
 echo "Running pip3 install..."
 pip3 install --quiet --no-input -r requirements.txt
 echo "Finished running pip3 install"
-if [ ! -f "sage-configured.txt" ] then;
-    touch "sage-configured.txt"
-fi
+
 
 # the meat of the matter
 clear
